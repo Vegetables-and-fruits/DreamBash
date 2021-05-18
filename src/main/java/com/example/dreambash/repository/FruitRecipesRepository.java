d00b7119e24ee6c5a59a96715a4b7bfb207a8cb0
Meron-prog <72084362+Meron-prog@users.noreply.github.com>
2021-05-18 15:02:26 +0200

    added repository class

--- src/main/java/com/example/dreambash/repository/FruitRecipesRepository.java
+++ src/main/java/com/example/dreambash/repository/FruitRecipesRepository.java
@@ -1,4 +1,10 @@
 package com.example.dreambash.repository;
 
-public class FruitRecipesRepository {
+import com.example.dreambash.model.FruitRecipes;
+
+import java.util.List;
+
+public interface FruitRecipesRepository extends MongoRepository<FruitRecipes, String> {
+
+    List<FruitRecipes> findFruitRecipiesByNeedsOven(boolean needsOven );
 }
