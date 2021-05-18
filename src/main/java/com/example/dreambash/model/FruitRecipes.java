22a5a36d3bc15325fe6d5a40b5627e4b2538e2c7
SanaJava <sana.eneroth@gmail.com>
2021-05-18 14:23:20 +0200

    created fruit recipes model class

--- src/main/java/com/example/dreambash/model/FruitRecipes.java
+++ src/main/java/com/example/dreambash/model/FruitRecipes.java
@@ -1,4 +1,16 @@
 package com.example.dreambash.model;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class FruitRecipes {
+    String id;
+    String name;
+    String description;
+    int servings;
+    boolean hasRecipes;
+    boolean needsOven;
+
 }
