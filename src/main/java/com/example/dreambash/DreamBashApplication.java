7f8459bfc4a32fcc849fa3c4ff605ed14c9732e1
SanaJava <sana.eneroth@gmail.com>
2021-05-18 10:54:19 +0200

    Initial commit

---
+++ src/main/java/com/example/dreambash/DreamBashApplication.java
@@ -0,0 +1,13 @@
+package com.example.dreambash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamBashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamBashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dreambash/DreamBashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dreambash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamBashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
