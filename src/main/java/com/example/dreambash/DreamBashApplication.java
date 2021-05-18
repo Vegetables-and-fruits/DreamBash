414af51c4d0c9c55eb93d8f1517873d699abc572
irynagnatenko <ira.gnatenko@gmail.com>
2021-05-18 14:41:27 +0200

    created a controller class for food recipes

--- src/main/java/com/example/dreambash/DreamBashApplication.java
+++ src/main/java/com/example/dreambash/DreamBashApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DreamBashApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DreamBashApplication.class, args);
+    public static void main(String[] args) {SpringApplication.run(DreamBashApplication.class, args);
     }
 
 }
