bd3414a054f253b9e7f5f661590506e309b1d4ee
irynagnatenko <72083669+irynagnatenko@users.noreply.github.com>
2021-05-18 14:49:03 +0200

    created a controller class for food recipes (#2)

--- src/main/java/com/example/dreambash/controller/FruitRecipesController.java
+++ src/main/java/com/example/dreambash/controller/FruitRecipesController.java
@@ -1,5 +1,34 @@
 package com.example.dreambash.controller;
 
+import com.example.dreambash.model.FruitRecipes;
+import com.example.dreambash.service.FruitRecipesService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
 public class FruitRecipesController {
+
+    private final FruitRecipesService service;
+
+    @GetMapping("/recipes")
+    public List<FruitRecipes> getFruitRecipes() {
+        return service.getFruitRecipe();
+    }
+
+    @PostMapping("/saveRecipe")
+    public void saveNewFruitRecipe(@RequestBody FruitRecipes fruitRecipe) {
+        service.saveNewFruitRecipe(fruitRecipe);
+    }
+
+    @GetMapping("/recipes/needsOven")
+    public List<String> getRecipeThatNeedsOven() {
+        return service.getRecipeNeedsOven();
+    }
 
 }
