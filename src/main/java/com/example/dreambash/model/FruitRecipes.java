ec50fc4f214dc7e22bdfe54cff7af167f430777b
Yohannes2010 <abebejhon@yahoo.com>
2021-05-18 20:21:17 +0200

    FruitRecipesService added

--- src/main/java/com/example/dreambash/model/FruitRecipes.java
+++ src/main/java/com/example/dreambash/model/FruitRecipes.java
@@ -1,8 +1,9 @@
 package com.example.dreambash.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class FruitRecipes {
