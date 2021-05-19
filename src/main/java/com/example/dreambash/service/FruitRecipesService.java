182d77e9179d9e0b86106678cf5f5fefa4f04373
Yohannes2010 <72087660+Yohannes2010@users.noreply.github.com>
2021-05-19 20:45:51 +0200

    FruitRecipesService added (#4)

--- src/main/java/com/example/dreambash/service/FruitRecipesService.java
+++ src/main/java/com/example/dreambash/service/FruitRecipesService.java
@@ -1,4 +1,31 @@
 package com.example.dreambash.service;
 
+import com.example.dreambash.model.FruitRecipes;
+import com.example.dreambash.repository.FruitRecipesRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
 public class FruitRecipesService {
+    private final FruitRecipesRepository recipesRepository;
+
+    public List<FruitRecipes> getFruitRecipe() {
+        return recipesRepository.findAll();
+    }
+
+    public void saveNewFruitRecipe(FruitRecipes fruitRecipes) {
+        recipesRepository.save(fruitRecipes);
+    }
+
+    public List<String> getRecipeNeedsOven() {
+        List<FruitRecipes> receipNeedsOven = recipesRepository.findFruitRecipiesByNeedsOven(true);
+
+        return receipNeedsOven.stream()
+                .map(fruitRecipes -> fruitRecipes.getName())
+                .collect(Collectors.toList());
+    }
 }
