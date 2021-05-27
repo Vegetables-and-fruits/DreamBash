2022198118d6b53dd903afaf9d6668c2fcfa0bb9
Meron-prog <72084362+Meron-prog@users.noreply.github.com>
2021-05-27 10:03:47 +0200

    Added Unit tests for updateFruitRecipe in FruitRecipesService. And moved the logic from the controller to the service. (#10)
    Co-authored-by: SanaJava <71931392+SanaJava@users.noreply.github.com>

--- src/test/java/com/example/dreambash/service/FruitRecipesServiceTest.java
+++ src/test/java/com/example/dreambash/service/FruitRecipesServiceTest.java
@@ -2,11 +2,15 @@ package com.example.dreambash.service;
 
 import com.example.dreambash.model.FruitRecipes;
 import com.example.dreambash.repository.FruitRecipesRepository;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 
 import java.util.Collections;
 import java.util.List;
@@ -98,5 +102,26 @@ class FruitRecipesServiceTest {
 
     }
 
-}
+    @Test
+    public void ifUpdatingWithWrongId_returnsNull() {
+        FruitRecipesService fruitRecipesServiceNoMockRepository = new FruitRecipesService(fruitRecipesRepository);
+        fruitRecipesRepository.save(new FruitRecipes("1", "Recipe 1", "Description 1", 6, true, true));
+        FruitRecipes fruitRecipes = fruitRecipesServiceNoMockRepository.updateFruitRecipe("FelId", "Bra namn", null,null,null,null);
+        assertNull(fruitRecipes);
+    }
 
+    @Test
+    public void ifUpdatingWithWrongCorrectId_returnsUpdatedFruitRecipes() {
+        FruitRecipesService fruitRecipesServiceNoMockRepository = new FruitRecipesService(fruitRecipesRepository);
+        fruitRecipesRepository.save(new FruitRecipes("1", "Recipe 1", "Description 1", 6, true, true));
+        FruitRecipes fruitRecipes = fruitRecipesServiceNoMockRepository.updateFruitRecipe("1", "Bra namn", null,4,null,false);
+        assertNotNull(fruitRecipes);
+        assertTrue(fruitRecipes.getId().equals("1"));
+        assertTrue(fruitRecipes.getName().equals("Bra namn"));
+        assertTrue(fruitRecipes.getDescription().equals("Description 1"));
+        assertTrue(fruitRecipes.getServings() == 4);
+        assertTrue(fruitRecipes.isHasRecipes());
+        assertFalse(fruitRecipes.isNeedsOven());
+    }
+
+}
